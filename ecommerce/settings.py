e37f101eefc079fbd25babd82e1dad14b37d5674
Jana-script <jana.jscript@gmail.com>
2024-12-26 12:53:31 +0100

    su

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static_src']
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = '/media/'
 
