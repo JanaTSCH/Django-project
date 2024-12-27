81928f2769e306721e6390e4505cd658b0e8b8d1
Jana <jana.jscript@gmail.com>
2024-12-27 11:29:54 +0100

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -19,9 +19,9 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['django-project-gyni.onrender.com', '*']
+ALLOWED_HOSTS = ['django-app-ple6.onrender.com', '*']
 
-CSRF_TRUSTED_ORIGINS = ['https://django-project-gyni.onrender.com']
+CSRF_TRUSTED_ORIGINS = ['https://django-app-ple6.onrender.com']
 
 
 # Application definition
